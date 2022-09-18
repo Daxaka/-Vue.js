8a9561fcce2d80e2561972fadd1dfbe599feae81
Shalikin Ilya <97974980+Daxaka@users.noreply.github.com>
2022-09-18 18:05:36 +0700

    Master

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <MyCalculator msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import MyCalculator from './components/MyСanculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyCalculator
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+}
+.container{
+  max-width: 600px;
+  margin: auto;
+}
+.title{
+  text-align: center;
+  margin: 60px 0px;
+}
+.condition-wrap{
+  height: 50px;
+  width: 100%;
+}
+.calculation-btn{
+  width: 100%;
+  height: 60px;
+  font-size: 26px;
+  background: beige;
+  border: 1px solid #6355e53b;
+  cursor: pointer;
+}
+.calculator-wrap{
+  display: flex;
+}
+</style>
---
+++ src/components/MyСanculator.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <h1 class="title">Калькулятор</h1>
+    <div class="calculator-wrap">
+    <input class="condition-wrap" v-model.number="conditionOne" type="number">
+    <input class="condition-wrap" v-model.number="conditionTu" type="number">
+    <input class="condition-wrap" v-model.number="result" type="number">
+    </div>
+    <div class="calculator-wrap">
+    <button @click="addition" class="calculation-btn">+</button>
+    <button @click="subtraction" class="calculation-btn">-</button>
+    <button @click="division" class="calculation-btn">/</button>
+    <button @click="multiplication" class="calculation-btn">*</button>
+    <button @click="step" class="calculation-btn">^</button>
+    <button @click="wholeDivision" class="calculation-btn">(\)</button>
+    </div>
+    <p v-if="showTxt">Введите пожалуйста число</p>
+    <p v-if="zero">На ноль делить нельзя!!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCalcuLator',
+  data: () => ({
+    conditionOne: 0,
+    conditionTu: 0,
+    result: '',
+    showTxt: false,
+    zero: false
+  }),
+  props: {
+    msg: String
+  },
+  methods: {
+    addition () {
+      if (typeof this.conditionOne !== 'number' || typeof this.conditionTu !== 'number') {
+        this.showTxt = true
+        console.log(typeof this.result)
+      } else {
+        this.showTxt = false
+        this.zero = false
+        this.result = this.conditionOne + this.conditionTu
+      }
+    },
+    subtraction () {
+      if (typeof this.conditionOne !== 'number' || typeof this.conditionTu !== 'number') {
+        this.showTxt = true
+      } else {
+        this.showTxt = false
+        this.zero = false
+        this.result = this.conditionOne - this.conditionTu
+      }
+    },
+    division () {
+      if (typeof this.conditionOne !== 'number' || typeof this.conditionTu !== 'number') {
+        this.showTxt = true
+      } if (this.conditionOne === 0 || this.conditionTu === 0) {
+        this.zero = true
+      } else {
+        this.showTxt = false
+        this.zero = false
+        this.result = this.conditionOne / this.conditionTu
+      }
+    },
+    multiplication () {
+      if (typeof this.conditionOne !== 'number' || typeof this.conditionTu !== 'number') {
+        this.showTxt = true
+      } else {
+        this.showTxt = false
+        this.zero = false
+        this.result = this.conditionOne * this.conditionTu
+      }
+    },
+    step () {
+      if (typeof this.conditionOne !== 'number' || typeof this.conditionTu !== 'number') {
+        this.showTxt = true
+      } else {
+        this.showTxt = false
+        this.zero = false
+        this.result = this.conditionOne ** this.conditionTu
+      }
+    },
+    wholeDivision () {
+      if (typeof this.conditionOne !== 'number' || typeof this.conditionTu !== 'number') {
+        this.showTxt = true
+      } if (this.conditionOne === 0 || this.conditionTu === 0) {
+        this.zero = true
+      } else {
+        this.showTxt = false
+        this.zero = false
+        this.result = Math.floor(this.conditionOne / this.conditionTu)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
